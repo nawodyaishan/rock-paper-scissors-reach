272bba78e3768c1eb5bb4a2728216a7811e4f45c
nawodyaishan <nawodyain@gmail.com>
2023-04-25 23:32:54 +0530

    feat: reformat

--- index.mjs
+++ index.mjs
@@ -5,8 +5,7 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const [accAlice, accBob] =
-    await stdlib.newTestAccounts(2, startingBalance);
+const [accAlice, accBob] = await stdlib.newTestAccounts(2, startingBalance);
 console.log('Hello, Alice and Bob!');
 
 console.log('Launching...');
@@ -14,15 +13,10 @@ const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
 console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
-]);
+await Promise.all([backend.Alice(ctcAlice, {
+    ...stdlib.hasRandom, // implement Alice's interact object here
+}), backend.Bob(ctcBob, {
+    ...stdlib.hasRandom, // implement Bob's interact object here
+}),]);
 
 console.log('Goodbye, Alice and Bob!');
